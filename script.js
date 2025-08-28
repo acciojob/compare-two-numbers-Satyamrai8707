c4b4e15c20f461242d17b2457b090621bc7fdf7c
Satyam Rai <114270778+Satyamrai8707@users.noreply.github.com>
2025-08-28 14:25:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 
 let num1 = prompt("Enter first num")
 let num2 = prompt("Enter second num")
-if (num1 === num2) {
-	return true
-}
-else{
-	return false
-}
+
+let result = num1 === num2
+alert(result)
