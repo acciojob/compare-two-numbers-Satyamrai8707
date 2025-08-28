ff39440935aa53a1812132b6dcf8dde814ad9066
Satyam Rai <114270778+Satyamrai8707@users.noreply.github.com>
2025-08-28 14:23:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+//your JS code here. If requiredlet 
+
+let num1 = prompt("Enter first num")
+let num2 = prompt("Enter second num")
+if (num1 === num2) {
+	return true
+}
+else{
+	return false
+}
